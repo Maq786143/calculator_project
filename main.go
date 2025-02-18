1739cb5187249989ef315938aa9b0ecf602b2026
Maqbool Sheikh <maqbool.sheikh1997@gmail.com>
2025-02-18 18:24:29 -0500

    Added main.go with calculator functionality

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	num1, num2 := 10.0, 5.0
+
+	fmt.Println("Addition:", calculator.Add(num1, num2))
+	fmt.Println("Subtraction:", calculator.Subtract(num1, num2))
+	fmt.Println("Multiplication:", calculator.Multiply(num1, num2))
+
+	result, err := calculator.Divide(num1, num2)
+	if err != nil {
+		fmt.Println("Error:", err)
+	} else {
+		fmt.Println("Division:", result)
+	}
+}
